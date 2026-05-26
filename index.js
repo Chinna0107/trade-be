69f18c59f026d86ba7f01413457e56897abbf43c
chinna0107 <kancharlahemanth89@gmail.com>
2026-05-26 14:33:57 +0530

    Production 2.32

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = (process.env.FRONTEND_URLS || process.env.FRONTEND_URL || [
-  'http://localhost:5173',
-  'https://tradenexustradesmart.com',
-].join(','))
-  .split(',')
+const configuredOrigins = [
+  process.env.FRONTEND_DEV_URL,
+  process.env.FRONTEND_PROD_URL,
+  process.env.FRONTEND_URL,
+  ...(process.env.FRONTEND_URLS || '').split(','),
+];
+
+const allowedOrigins = (configuredOrigins.some(Boolean)
+  ? configuredOrigins
+  : [
+      'http://localhost:5173',
+      'https://tradenexustradesmart.com',
+    ])
+  .flatMap((origin) => (origin || '').split(','))
   .map((origin) => origin.trim().replace(/\/$/, ''))
   .filter(Boolean);
 
